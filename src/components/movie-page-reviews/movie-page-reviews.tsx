d6000b709460daaf42c1471f0cba19d95cb7e84f
Савчиц Алексей С <as.savchits@jet.su>
2023-01-19 00:41:21 +0300

    module5-task2

--- src/components/movie-page-reviews/movie-page-reviews.tsx
+++ src/components/movie-page-reviews/movie-page-reviews.tsx
@@ -9,37 +9,44 @@ function MoviePageReviews({
   reviews,
   filmId,
 }: MoviePageReviewsProps): JSX.Element {
-  return (
-    <>
-      {reviews.map(
-        (review, index) =>
-          review.id === filmId && (
-            <div
-              key={`${index.toString()} - ${review.id.toString()}`}
-              className="film-card__reviews-col"
-            >
-              <div className="review">
-                <blockquote className="review__quote">
-                  <p className="review__text">{review.comment}</p>
-                  <footer className="review__details">
-                    <cite className="review__author">{review.user.name}</cite>
-                    <time
-                      className="review__date"
-                      dateTime={new Date(
-                        new Date(review.date).toISOString()
-                      ).toDateString()}
-                    >
-                      {review.date}
-                    </time>
-                  </footer>
-                </blockquote>
+  const gridCols: JSX.Element[][] = [[], []];
 
-                <div className="review__rating">{review.rating}</div>
-              </div>
-            </div>
-          )
-      )}
-    </>
+  const movieReviews = reviews.filter((review) => review.id === filmId);
+
+  movieReviews.forEach((review, index) => {
+    const reviews = (
+      <div
+        className="review"
+        key={`${index.toString()} - ${review.id.toString()}`}
+      >
+        <blockquote className="review__quote">
+          <p className="review__text">{review.comment}</p>
+          <footer className="review__details">
+            <cite className="review__author">{review.user.name}</cite>
+            <time
+              className="review__date"
+              dateTime={new Date(
+                new Date(review.date).toISOString()
+              ).toDateString()}
+            >
+              {review.date}
+            </time>
+          </footer>
+        </blockquote>
+
+        <div className="review__rating">{review.rating}</div>
+      </div>
+    );
+
+    const colNumber = index % 2;
+    gridCols[colNumber].push(reviews);
+  });
+
+  return (
+    <div className="film-card__reviews film-card__row">
+      <div className="film-card__reviews-col">{gridCols[0]}</div>
+      <div className="film-card__reviews-col">{gridCols[1]}</div>
+    </div>
   );
 }
 
